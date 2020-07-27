755dd600c72781c251f3a2728739766209eede7a
sarvnazale <55462526+sarvnazale@users.noreply.github.com>
2020-07-27 02:46:34 -0400

    Update SpaceWars.java

--- SpaceWars.java
+++ SpaceWars.java
@@ -1,30 +1,48 @@
-package ISU;
+package com.SPACEWARS;
+/**created by 
+ * Sarvnaz Alemohammad
+ */
+
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+import java.awt.Graphics;
 
-public class SpaceWars extends JFrame{
+public class SpaceWars extends JFrame  {
+
+    private static final long serialVersionUID = 1L;
+    public Graphics g;
+    /**
+     * constructor
+     * pre: none
+     * post: initializes user interface
+     */
     public SpaceWars() {
 
         initUI();
     }
-    
+    /**
+     * creates new Board and starts the game
+     * pre: none
+     * post: basics of game have been created
+     */
+
     private void initUI() {
+        
+        add(new GamePlay(g));
 
-        //add(new Board());
-
-        setTitle("Space Invaders");
-        setSize(Constants.BOARD_WIDTH, Constants.BOARD_HEIGHT);
-
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setResizable(false);
+        setTitle("Space Wars"); //title of page
+        setSize(Constants.BOARD_WIDTH, Constants.BOARD_HEIGHT); //size of page
         setLocationRelativeTo(null);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //exists the game when tab is closed
+        setResizable(false); //page is not resizable
     }
 
     public static void main(String[] args) {
+        
         EventQueue.invokeLater(() -> {
-
-            var ex = new SpaceWars();
-            ex.setVisible(true);
+            var ex = new SpaceWars(); //new spaceinvaders game
+            ex.setVisible(true); 
         });
-    }	
+    }
 }
